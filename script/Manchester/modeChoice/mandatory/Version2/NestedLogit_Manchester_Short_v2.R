bea5770192a4fd69921e76b091ab2c023ba2f53c
Tayebeh Saghapour <tayebeh.saghapour@rmit.edu.au>
2023-07-04 11:31:46 +1000

    Modified

--- script/Manchester/modeChoice/mandatory/Version2/NestedLogit_Manchester_Short_v2.R
+++ script/Manchester/modeChoice/mandatory/Version2/NestedLogit_Manchester_Short_v2.R
@@ -5,7 +5,7 @@ rm(list = ls())
 #rm(list= ls()[!(ls() %in% c('modelmnl','modelsht','modelsht2'))])
 
 ### Load Apollo library
-library(apollo)
+library(apollO)
 suppressPackageStartupMessages(library(dplyr)) # for manipulating data
 
 # ################################################################# #
